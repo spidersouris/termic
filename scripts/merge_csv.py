14b9d4fc37187bff2c4f0bb94b08ab0a9ab48f0d
Enzo Doyen <spidersouris@gmail.com>
2023-04-29 20:22:12 +0200

    Added Dutch for testing purposes

--- scripts/merge_csv.py
+++ scripts/merge_csv.py
@@ -3,18 +3,21 @@ import glob
 
 df_list = []
 
-csv_files = glob.glob("csv/*.csv")
+lang = "nl-NL"
+csv_files = glob.glob(f"../csv/{lang}/*.csv")
 
 # todo: review implementation / this is garbage but it works
-final_df = pd.read_csv(csv_files[0], skiprows=12, delimiter=',', encoding='utf-8', error_bad_lines=False)
+final_df = pd.read_csv(csv_files[0], skiprows=13, delimiter=',', encoding='utf-8', error_bad_lines=False)
+final_df = final_df.loc[:, ~final_df.columns.str.contains('^Unnamed')]
+final_df.to_csv(f"../data/merged_exc_{lang}.csv", mode="w", sep="\t", index=False)
 
 for csv_file in csv_files[1:]:
-    df = pd.read_csv(csv_file, skiprows=12, delimiter=',', encoding='utf-8', error_bad_lines=False)
+    df = pd.read_csv(csv_file, skiprows=13, delimiter=',', encoding='utf-8', error_bad_lines=False)
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-    df_list.append(df)
+    df.to_csv(f"../data/merged_exc_{lang}.csv", mode="a", sep="\t", index=False)
 
-#print(df_list)
+# print(df_list)
 
-merged_df = pd.concat(df_list, axis=0, ignore_index=True)
+#merged_df = pd.concat([final_df.reset_index(drop=True), final_df2.reset_index(drop=True)], axis=0)
 
-merged_df.to_csv("data/merged.csv", sep="\t", index=False)
+
--- search.py
+++ search.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 app = Flask(__name__)
 
-df_gl = pd.read_excel("data/glossary.xlsx")
-df_exc = pd.read_csv("data/merged_exc.csv", sep="\t")
+#df_exc = pd.read_csv("data/merged_exc.csv", sep="\t")
+#df_exc_nlNL = pd.read_csv("data/merged_exc_nl-NL.csv", sep="\t")
 
 @app.route("/")
 def index():
@@ -14,11 +14,12 @@ def index():
 def main():
     term = request.json["term"]
     target_lang = request.json["target_lang"]
+    df_gl = pd.read_excel(f"data/glossary_{target_lang}.xlsx")
+    df_exc = pd.read_csv(f"data/merged_exc_{target_lang}.csv", sep="\t")
 
-    df_gl.drop(df_gl.columns[df_gl.columns.str.contains("unnamed", case=False)], axis=1, inplace=True)
+    df_gl.drop(df_gl.columns[df_gl.columns.str.contains("unnamed", case=False)], axis=1, inplace=True) # is it even necessary?
     results_gl = df_gl[df_gl["term_en-US"].str.contains(term, case=False, na=False)]
     results_exc = df_exc[df_exc["Source Term"].str.contains(term, case=False, na=False)]
-    #print(results_gl.head())
 
     if len(results_gl) > 0:
         gl_source = results_gl["term_en-US"].tolist()
@@ -27,7 +28,7 @@ def main():
         gl_target_pos = results_gl["pos_" + target_lang].str.split(n=1).str[1].str.lower().tolist()
         gl_source_def = [s[:1].lower() + s[1:] for s in results_gl["def_en-US"].str.split(n=1).str[1].tolist()]
     else:
-        gl_source, gl_translation, gl_source_pos, gl_target_pos, gl_source_def = []
+        gl_source, gl_translation, gl_source_pos, gl_target_pos, gl_source_def = [''] * 5
 
     if len(results_exc) > 0:
         exc_source = results_exc["Source Term"].tolist()
@@ -37,7 +38,7 @@ def main():
         exc_product = results_exc["Product"].fillna("unknown").tolist()
         exc_version = results_exc["Version"].fillna("unknown").tolist()
     else:
-        exc_source, exc_translation, exc_cat, exc_platform, exc_product, exc_version = []
+        exc_source, exc_translation, exc_cat, exc_platform, exc_product, exc_version = [''] * 6
     
     # JSON Response
     response = {"gl_source": gl_source, "gl_translation": gl_translation, "gl_source_pos": gl_source_pos, "gl_target_pos": gl_target_pos, "gl_source_def": gl_source_def,
